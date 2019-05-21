a5d6f8603abb419394aa9dad17772321785ce9cf
Eliot Howes <ejhowes@gmail.com>
2019-05-21 21:24:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n >= 1 do
+    if n == 12
+      puts "HAPPY NEW YEAR!"
+    else
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+  end
 end
